0893c44917537fd42e0a2853b2fa4d11efdaa362
katavouta <54277836+katavouta@users.noreply.github.com>
2019-11-19 13:35:37 +0000

    Update fix_elevation.sh

--- fix_elevation.sh
+++ fix_elevation.sh
@@ -6,7 +6,7 @@ module load cray-netcdf cray-hdf5
 module load nco/4.5.0
 
 #cp or download the gebco bathymetry
-cp $GITCLONE/FILES_START/DOMAIN/GRIDONE_2008_2D_74.0_-21.0_134.0_25.0.nc $DOMAIN
+cp $GITCLONE/DOMAIN/GRIDONE_2008_2D_74.0_-21.0_134.0_25.0.nc $DOMAIN
 
 #make sure you remove any elevation for land and fix the record
 ncap2 -s 'where(elevation > 0) elevation=0' GRIDONE_2008_2D_74.0_-21.0_134.0_25.0.nc tmp.nc
