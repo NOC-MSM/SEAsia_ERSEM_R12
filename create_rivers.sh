5e3d743b0f979119cc46964896b151c11eebe9de
katavouta <54277836+katavouta@users.noreply.github.com>
2019-11-19 13:52:30 +0000

    Update create_rivers.sh

--- create_rivers.sh
+++ create_rivers.sh
@@ -2,6 +2,6 @@ cd $RIVER
 
 # copy the rivers but see description in the wiki on how they have
 #been generated
-cp $GITCLONE/RIVERS/SEAsia_rivertest_v3.nc $RIVER
+cp /work/n01/n01/jenjar93/accord/SEAsia_rivertest_v3.nc $RIVER
 
 cd $WORK
