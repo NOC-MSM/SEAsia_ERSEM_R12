f60c7a50817017c41077ad94dd3e0b4162baf884
katavouta <54277836+katavouta@users.noreply.github.com>
2019-11-20 14:46:35 +0000

    Update get_bathymetry.sh

--- get_bathymetry.sh
+++ get_bathymetry.sh
@@ -22,8 +22,13 @@ module unload cray-netcdf-hdf5parallel cray-hdf5-parallel
 module load cray-netcdf cray-hdf5
 module load nco/4.5.0
 
+#remove weirdness with negative bathymetry and make minimum bathymetry
+#equal to 10 m (resolve any possible wet-drying problems)
+ncap2 -s 'where(Bathymetry < 0) Bathymetry=0' bathy_meter.nc tmp1.nc
+ncap2 -s 'where(Bathymetry < 10 && Bathymetry > 0) Bathymetry=10' tmp1.nc -O bathy_meter.nc
+rm tmp1.nc
 
 #copy it if you want for safe keeping
-cp bathy_meter.nc bathy_meter_gebco.nc
+cp bathy_meter.nc bathy_meter_ORCA12.nc
 
 cd $WORK
