03637d71d0ee81d8dad6fa548c63de1e1d1a5ce2
katavouta <54277836+katavouta@users.noreply.github.com>
2019-11-20 14:03:58 +0000

    Update main_setup.sh

--- main_setup.sh
+++ main_setup.sh
@@ -11,10 +11,8 @@ echo "Compiling various grid tools"
 . ./make_tools.sh                                >> main_output.txt 2>&1
 echo "Creating coordinate file"
 . ./create_coordinates.sh                        >> main_output.txt 2>&1
-echo "Removing land"
-. ./fix_elevation.sh                             >> main_output.txt 2>&1
-echo "Interpolating GEBCO on to our domain"
-. ./interpolate_gebco.sh                         >> main_output.txt 2>&1
+echo "Interpolating bathymetry on to our domain"
+. ./interpolate_bathymetry.sh                         >> main_output.txt 2>&1
 echo "Create rivers"
 . ./create_rivers.sh                            >> main_output.txt 2>&1
 echo "Create domain file"
