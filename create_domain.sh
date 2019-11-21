9f620ac76a9c3c2bf4edd9d6392904a534049db7
katavouta <54277836+katavouta@users.noreply.github.com>
2019-11-21 15:20:23 +0000

    Update create_domain.sh

--- create_domain.sh
+++ create_domain.sh
@@ -2,9 +2,9 @@ cd $TDIR
 
 #load modules
 module unload nco cray-netcdf cray-hdf5
-module load cray-netcdf-hdf5parallel/4.4.1.1
-module load cray-hdf5-parallel/1.10.0.1
-module swap PrgEnv-cray PrgEnv-intel/5.2.82
+module swap PrgEnv-cray PrgEnv-intel
+module load cray-netcdf-hdf5parallel
+module load cray-hdf5-parallel
 
 #take namelist (!!modify it if necessary) 
 cp $GITCLONE/DOMAIN/namelist_cfg $TDIR/DOMAINcfg/namelist_cfg
