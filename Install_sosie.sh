b9b2442c311e1a6a64959b14725d8ec51cc6fb15
katavouta <54277836+katavouta@users.noreply.github.com>
2019-12-13 17:15:03 +0000

    Update Install_sosie.sh

--- Install_sosie.sh
+++ Install_sosie.sh
@@ -10,9 +10,8 @@ cd sosie
 # you may need to edit the path in this file
 # to add the directory to install binaries
 cp $GITCLONE/IC/make.macro make.macro
-
-# Directory to install binaries:
-INSTALL_DIR = /home/n01/n01/annkat/local
+# !!ATTENTION!! inside make.macro change the directory to install into 
+# (where username is your username): INSTALL_DIR = /home/n01/n01/username/local
 
 make
 make install
